c231918d3dac79d23676ca77c6d9187ed2a6d8e0
n0tybynature <74768895+n0tybynature@users.noreply.github.com>
2021-02-08 15:51:13 -0800

    submit button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
+import Todo from "./components/Todo"
+
+const todos = [
+  {
+    
+  }
+]
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  // constructor (){
+  //   super();
+  //   this.state = {
+  //     todolist={todos}
+  //   }
+  // }
+
+  handleItemToggle = () => {
+    const newTodo = this.state.todolist
+    newTodo[0].done = true;
+    this.setState({
+      todolist: newTodo
+    })
+  }
+
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Todo List: MVP!</h2>
+        <TodoForm/>
       </div>
     );
   }
---
+++ src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+
+
+
+
+
+const Todo = props => {
+    return {
+        <div className={`todo${props.todo.done ? 'done': ''}`}>
+    }
+}
+
+export default Todo;
